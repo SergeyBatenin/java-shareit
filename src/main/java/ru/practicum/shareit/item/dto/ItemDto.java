a10c7fb103f10aa5f10116ee5e19b4abbedc6508
Sergey Batenin <inferno.87@mail.ru>
2024-08-06 08:19:13 +0300

    add validation annotations

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,8 +11,10 @@ import lombok.Data;
 public class ItemDto {
     private Long id;
     @NotBlank
+    @Size(min = 1, max = 255)
     private String name;
     @NotBlank
+    @Size(min = 1, max = 1000)
     private String description;
     @NotNull
     private Boolean available;
